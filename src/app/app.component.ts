e3467e6c6e2f886a34f6b96779d746f39198a6ba
Joel Dowono <joeldowono1@gmail.com>
2022-11-14 09:33:39 +0100

    modification du template app.component.html pour afficher la liste des pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokemon qui n'eziste pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
