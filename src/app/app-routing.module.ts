a13a9a822391b3880175c884f8c7d8fe1ac93b74
Dowono Joel <joeldowono1@gmail.com>
2022-11-16 21:13:16 +0100

    mise en place du module de fonctionnalité pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
 const routes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemon/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,19 @@
+import { PokemonModule } from './pokemon/pokemon.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { RouterModule } from '@angular/router';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
